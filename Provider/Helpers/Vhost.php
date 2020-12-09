182a64d89582d877b3a4eb567496a170cc55446d
Anton Boutkam <anton@nui-boutkam.nl>
2020-12-09 11:02:49 +0100

    Added SSL directives P1

--- Provider/Helpers/Vhost.php
+++ Provider/Helpers/Vhost.php
@@ -5,6 +5,7 @@ namespace Provider\Helpers;
 class Vhost
 {
     private $sDomain, $sServerAdmin, $iPort, $sDocumentRoot, $sLogDirectory;
+    private bool $bUseSsl;
 
     function __construct(string $sServerAdmin, string $sDomain, int $iPort, string $sDocumentRoot, string $sLogdir, bool $bUseSSL = false)
     {
@@ -13,6 +14,7 @@ class Vhost
         $this->iPort = $iPort;
         $this->sDocumentRoot = $sDocumentRoot;
         $this->sLogDirectory = $sLogdir;
+        $this->bUseSsl = $bUseSSL;
     }
 
     function getContents()
@@ -22,6 +24,15 @@ class Vhost
         {
             $sServerAdmin = PHP_EOL .'ServerAdmin ' . $this->sServerAdmin;
         }
+
+        $aAddUseSsl = [];
+        if($this->bUseSsl){
+            $aAddUseSsl[] = "SSLCertificateFile /app/data/CertBot/live/{$this->sDomain}/fullchain.pem";
+            $aAddUseSsl[] = "SSLCertificateKeyFile /app/data/CertBot/live/{$this->sDomain}/privkey.pem";
+            // $aAddUseSsl[] = "Include /etc/letsencrypt/options-ssl-apache.conf";
+        }
+
+        $sUseSSl = join(PHP_EOL, $aAddUseSsl);
 
         $sSep = DIRECTORY_SEPARATOR;
         return <<<VHOST
@@ -37,7 +48,7 @@ class Vhost
 # See https://gitlab.com/NovumGit/innovation-app-util-http for detailed information about
 # these config files.
 # 
-
+{($this->bUseSsl ? '<IfModule mod_ssl.c>' : '')}
 <VirtualHost *:{$this->iPort}>
 ServerName {$this->sDomain}{$sServerAdmin}
 DocumentRoot {$this->sDocumentRoot}
@@ -49,7 +60,9 @@ Require all granted
 ErrorLog {$this->sLogDirectory}{$sSep}{$this->sDomain}.error.log
 CustomLog {$this->sLogDirectory}{$sSep}{$this->sDomain}.access.log combined
 
-</VirtualHost>
+</IfModule>
+{($this->bUseSsl ? '<IfModule mod_ssl.c>' : '')}
+
 VHOST;
     }
 }
